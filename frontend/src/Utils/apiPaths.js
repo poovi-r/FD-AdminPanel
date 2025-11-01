efe88ad87e84a6e66b3dc0eb487906575f6fbf6f
Poovarasan R <poovarasan.mern@gmail.com>
2025-11-01 14:49:16 +0530

    final

--- frontend/src/Utils/apiPaths.js
+++ frontend/src/Utils/apiPaths.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://fd-admin-backend.onrender.com/",
+  baseURL: "http://localhost:5000/",
   withCredentials: true,
 });
 
